c47890a620a6f450b4e242d6293790cf2e59af0d
Carlo <carlohagens@outlook.com>
2022-03-16 19:28:36 +0000

    properly updated to dotnet 6

--- MergeQueue.Api/Program.cs
+++ MergeQueue.Api/Program.cs
@@ -1,20 +1,58 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using MergeQueue.Api;
+using MergeQueue.Api.Filters;
+using MergeQueue.Api.Repositories;
+using MergeQueue.Api.Settings;
+using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
-namespace MergeQueue.Api
+var builder = WebApplication.CreateBuilder(args);
+var version = "v1.4.1";
+
+// Add services to the container.
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+builder.Services.AddSingleton<IMongoClient>(_ =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+builder.Services.AddScoped<AuthenticationFilter>();
+builder.Services.AddSingleton<IQueueRepository, MongoDbQueueRepository>();
+builder.Services.AddSingleton<HttpClient, HttpClient>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true)
+                .AddJsonOptions(options =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy();
                 });
-    }
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(version, new OpenApiInfo { Title = "MergeQueue", Version = version });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint($"/swagger/{version}/swagger.json", $"MergeQueue {version}"));
 }
+
+app.UseRouting();
+app.Use((context, next) =>
+{
+    context.Request.EnableBuffering();
+    return next();
+});
+
+app.MapControllers();
+
+app.Run();
